e63e5fd9fead4c2d5728e44870735e58beca992f
Alvaro Carballo <alv.carballo@outlook.es>
2021-01-29 01:52:40 +0100

    Final

---
+++ app/src/main/java/es/studium/botonera/ui/extra/Extra.java
@@ -0,0 +1,38 @@
+package es.studium.botonera.ui.extra;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.studium.botonera.R;
+
+public class Extra extends Fragment {
+
+    private ExtraViewModel mViewModel;
+
+    public static Extra newInstance() {
+        return new Extra();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.extra_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ExtraViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/es/studium/botonera/ui/extra/ExtraViewModel.java
@@ -0,0 +1,7 @@
+package es.studium.botonera.ui.extra;
+
+import androidx.lifecycle.ViewModel;
+
+public class ExtraViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
